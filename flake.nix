45082547df26bdeae32a3b680ceff990c57584c9
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-03-28 12:14:49 +0530

    then we made a polygon .w.

--- flake.nix
+++ flake.nix
@@ -16,11 +16,32 @@
         system:
           f (import nixpkgs {inherit system;})
       );
+    love2d = {
+      stdenv,
+      fetchFromGitHub,
+    }:
+      stdenv.mkDerivation {
+        name = "LuaCats-love2d";
+        src = fetchFromGitHub {
+          owner = "LuaCats";
+          repo = "love2d";
+          rev = "765abafe49775ba52937430e2546dfc330381d6b";
+          hash = "sha256-IRG/v8NcelqXWKovXm8d5YAAkCUb+6WvgmioaSUso/c=";
+        };
+
+        installPhase = ''
+          mkdir $out
+          cp -r $src/library $out
+        '';
+      };
   in {
+    packages = forAllSystems (pkgs: {
+      default = pkgs.callPackage love2d {};
+    });
     devShells = forAllSystems (pkgs: {
       default = pkgs.mkShell {
         buildInputs = [
-            pkgs.love
+          pkgs.love
         ];
       };
     });
--- main.lua
+++ main.lua
@@ -1,3 +1,48 @@
+local number = 1
+
+---@class Polygon
+---@field vertices [number, number][]
+local Polygon = {}
+Polygon.__index = Polygon
+
+---@param vertices [number, number][]
+---@return Polygon
+function Polygon:new(vertices)
+  self = setmetatable({}, Polygon)
+  self.vertices = vertices
+  return self
+end
+
+---@return number[]
+function Polygon:normalizedVertices()
+  local output = {}
+  for i = 1, #self.vertices do
+    output[#output + 1] = self.vertices[i][1]
+    output[#output + 1] = self.vertices[i][2]
+  end
+  return output
+end
+
+---@param value number
+---@param coordinate "x" | "y" | "both"
+function Polygon:update(coordinate, value)
+  for i = 1, #self.vertices do
+    if coordinate == "x" then
+      self.vertices[i][1] = self.vertices[i][1] + value
+    elseif coordinate == "y" then
+      self.vertices[i][2] = self.vertices[i][2] + value
+    else
+      self.vertices[i][1] = self.vertices[i][1] + value
+      self.vertices[i][2] = self.vertices[i][2] + value
+    end
+  end
+end
+
+local my_poly = Polygon:new({ { 50, 0 }, { 0, 100 }, { 100, 100 } })
+
 function love.draw()
-    love.graphics.print('Hello World!', 400, 300)
+  number = number + 1;
+  love.graphics.print(tostring(number), 300, number)
+  my_poly:update("both", 1)
+  love.graphics.polygon('line', my_poly:normalizedVertices())
 end
