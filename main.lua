8e3407d63a5db67243b1b1d7d935d45063e7cb6d
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-03-29 15:06:31 +0530

    add schlong

--- main.lua
+++ main.lua
@@ -1,11 +1,21 @@
 require('lib.polygon')
--- local my_poly = Polygon:new({ {100, 200}, {100, 100}, {200, 200}, {200, 100}})
--- local my_bow_tie = Polygon:new({ -- don't ask me what this is
---   {55, 45}, {100, 45}, {100, 100}, {90, 100}, {90, 55}, {55, 55}, {55, 100},
---   {0, 100}, {0, 90}, {45, 90}, {45, 55}, {0, 55}, {0, 0}, {10, 0}, {10, 45},
---   {45, 45}, {45, 0}, {100, 0}, {100, 10}, {55 , 10},
--- })
-local my_bow_tie = Polygon:new({{50, 0}, {100, 90}, {0,90}})
+local shapes = {
+  ["my_poly"] = Polygon:new({ { 100, 200 }, { 100, 100 }, { 200, 200 }, { 200, 100 } }),
+  ["triangle"] = Polygon:new({ { 50, 0 }, { 100, 90 }, { 0, 90 } }),
+  ["tesla"] = Polygon:new({ -- don't ask me what this is
+    { 55, 45 }, { 100, 45 }, { 100, 100 }, { 90, 100 }, { 90, 55 }, { 55, 55 }, { 55, 100 },
+    { 0,  100 }, { 0, 90 }, { 45, 90 }, { 45, 55 }, { 0, 55 }, { 0, 0 }, { 10, 0 }, { 10, 45 },
+    { 45, 45 }, { 45, 0 }, { 100, 0 }, { 100, 10 }, { 55, 10 },
+  }),
+  ["schlong"] = Polygon:new({
+    { 30, 150, }, { 30, 200, }, { 0, 200, }, { 0, 150, }, { 30, 150, },
+    { 30, 0, }, { 70, 0, }, { 70, 150, }, { 100, 150, }, { 100, 200, },
+    { 70, 200, }, { 70, 150, },
+  })
+}
+
+local my_polygon = shapes['schlong']
+local fill = 'line'
 
 local x = 0;
 local my_color = { 1, 0, 0 }
@@ -13,7 +23,7 @@ local my_color = { 1, 0, 0 }
 function love.draw()
   local graphics = love.graphics
   graphics.setColor(my_color)
-  graphics.polygon('fill', my_bow_tie:normalizedVertices())
+  graphics.polygon('line', my_polygon:normalizedVertices())
 end
 
 ---@param delta number
@@ -22,22 +32,22 @@ function love.update(delta)
   x = (x + 0.01) % 360
   local speed = 80;
   if love.keyboard.isDown("up") then
-    my_bow_tie:update("y", -speed * delta)
+    my_polygon:update("y", -speed * delta)
   end
   if love.keyboard.isDown("down") then
-    my_bow_tie:update("y", speed * delta)
+    my_polygon:update("y", speed * delta)
   end
   if love.keyboard.isDown("left") then
-    my_bow_tie:update("x", -speed * delta)
+    my_polygon:update("x", -speed * delta)
   end
   if love.keyboard.isDown("right") then
-    my_bow_tie:update("x", speed * delta)
+    my_polygon:update("x", speed * delta)
   end
   if love.keyboard.isDown("space") then
     if love.keyboard.isDown("lshift") then
-      my_bow_tie:rotate(5)
+      my_polygon:rotate(5)
     else
-      my_bow_tie:rotate(-5)
+      my_polygon:rotate(-5)
     end
   end
 end
